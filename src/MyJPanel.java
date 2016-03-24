8bec0de690b050aeac330cf69d2fd9c240ec9c94
NTHUCS <NTHUCS@140.114.86.233>
2016-03-24 16:03:56 +0800

    add fillRect() in paintComponent()

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -18,14 +18,12 @@ public class MyJPanel extends JPanel implements MouseMotionListener{
 	}
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		// TODO Draw square and fill it with random color decided by method getRandomColor()
 		// You should use fillRect()
-		
+		g.fillRect(centerX, centerY, square.getShapeWidth(), square.getShapeWidth());
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Get mouse dragged position and change suqare's position
 		centerX = e.getX();
 		centerY = e.getY();
 		repaint();
